0ea12a84d13eb825c130197f173a1d805b140aae
MikaelSoderstrom <mikael.soderstrom@live.se>
2014-10-24 19:38:01 +0200

    Moved routes to ./routes/index and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 node-proxy-boilerplate
 ======================
 
-Get started with a new proxy server in a minute!
+Tired of writing a new proxy server every time you need to mock a new backend, or just need a web server for your project? Clone node-proxy-boilerplate, and you are ready to go!
+
+## Usage
+
+### Hosting of Yeoman apps
+The following paths work if this server is placed in a folder parallell to the Yeoman app:
+
+	/the-folder-with-my-yeomanapp
+		/app
+			/scripts
+			/bower_components
+	/node-proxy-boilerplate
+
+#### Hosting of an unbuilt Yeoman app
+
+	app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp')));
+	app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp/app')));
+	app.use(express.static(path.join(__dirname, '../.tmp')));
+
+#### Hosting of a built Yeoman app
+
+	app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp/dist')));
--- index.js
+++ index.js
@@ -4,14 +4,9 @@ require('colors');
 var path = require('path');
 var morgan = require('morgan');
 var express = require('express');
+var routes = require('./routes/index.js');
 
 var app = express();
-
-// Routes
-var routes = {
-    api: require('./routes/api'),
-    hardcoded: require('./routes/hardcoded')
-};
 
 // Application Settings
 app.set('port', 4000);
@@ -19,27 +14,12 @@ app.set('backendUrl', 'http://localhost:3000');
 
 // Middlewares
 app.use(morgan('[:status] :method :url (:response-time ms)'));
+
+// Host static information
 app.use(express.static(path.join(__dirname, 'static')));
 
-// ****** Hosting of Yeoman apps ******
-// The following paths work if this server is placed in a folder parallell to the Yeoman app:
-// - the-folder-with-my-yeomanapp
-//              - app
-//                      - scripts
-//              - bower_components
-// - node-proxy-boilerplate
-
-// ****** Hosting of an unbuilt Yeoman app. ******
-// app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp')));
-// app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp/app')));
-// app.use(express.static(path.join(__dirname, '../.tmp')));
-
-// ****** Hosting of a built Yeoman app ******
-// app.use(express.static(path.join(__dirname, '../the-folder-with-my-yeomanapp/dist')));
-
 // Proxy request to backend
-app.use('/api', routes.api(app.get('backendUrl')));
-app.use('/hardcoded', routes.hardcoded);
+app.use('/', routes(app.get('backendUrl')));
 
 // Start server
 app.listen(app.get('port'), function () {
